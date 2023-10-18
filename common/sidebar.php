ffcd747e38b84e862fa03fdcaa637635b1c254f3
ednilsonmcs <ednilsonmcs@gmail.com>
2023-10-18 17:18:33 -0300

    adjust height sidebar and content

--- common/sidebar.php
+++ common/sidebar.php
@@ -1,13 +1,14 @@
 <nav class="child sidebar">
 
-    <div class="expand-button">
-        <!-- colocar um icone para expandir aqui -->
-        <span class="icon-link">
-            <img class="icon-link-image" src="images/list.svg" />
-        </span>
-    </div>
-
     <ul>
+        <li class="menu-item">
+            <a href="index.php">
+                <span class="icon-link">
+                    <img class="icon-link-image" src="images/list.svg" />
+                </span>
+                <span class="txt-link"></span>
+            </a>
+        </li>
         <li class="menu-item">
             <a href="index.php">
                 <span class="icon-link">
